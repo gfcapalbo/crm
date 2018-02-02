23122ec92a4976f46e354aa17471e23119c6f299
Ronald Portier <ronald@therp.nl>
2018-02-02 23:09:40 +0100

    [IMP] crm_claim_delivery. Style improvements.

--- crm_claim_delivery/models/product_template.py
+++ crm_claim_delivery/models/product_template.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2017 Therp BV <http://therp.nl>
+# Copyright 2017-2018 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openerp import api, fields, models
 
@@ -17,18 +17,15 @@ class ProductTemplate(models.Model):
         relation="product_selected_claim_rel",
         column1='claim_ids',
         column2='product_id',
-        string='Claims associated to this product'
-    )
+        string='Claims associated to this product')
     total_claims = fields.Integer(
         compute='_get_total_claims',
-        store=True,
-    )
+        store=True)
 
     @api.multi
     def action_view_claims(self):
         template_model = self.env['product.template']
         result = template_model._get_act_window_dict(
-            'crm_claim.crm_case_categ_claim0'
-        )
+            'crm_claim.crm_case_categ_claim0')
         result['domain'] = "[('id', 'in', %s)]" % self.claim_ids
         return result

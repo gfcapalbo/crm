e19030c075c4bbe887457a1c170793a4760f2223
Ronald Portier <ronald@therp.nl>
2018-02-02 22:54:27 +0100

    Revert "[FIX] Adds missing functionality, resolves #4090"
    This reverts commit 4b5a06884d1af664ebaec562eac07fdeef7709bb.
    
    The reverted commit introduced a dependency on claim.line and thereby
    rma that should not be there.

--- crm_claim_delivery/models/product_template.py
+++ crm_claim_delivery/models/product_template.py
@@ -8,28 +8,16 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     @api.multi
-    @api.depends('claim_ids')
     def _get_total_claims(self):
         for this in self:
             this.total_claims = len(this.claim_ids)
-
-    @api.multi
-    def _compute_claim_ids(self):
-        for rec in self:
-            claim_lines = self.env['claim.line'].search(
-                [('product_id.product_tmpl_id.id', '=', rec.id)])
-            claim_ids = set()
-            for line in claim_lines:
-                claim_ids.add(line.claim_id.id)
-            rec.claim_ids = [(6, 0, list(claim_ids))]
 
     claim_ids = fields.Many2many(
         comodel_name='crm.claim',
         relation="product_selected_claim_rel",
         column1='claim_ids',
         column2='product_id',
-        string='Claims associated to this product',
-        compute='_compute_claim_ids',
+        string='Claims associated to this product'
     )
     total_claims = fields.Integer(
         compute='_get_total_claims',
@@ -42,5 +30,5 @@ class ProductTemplate(models.Model):
         result = template_model._get_act_window_dict(
             'crm_claim.crm_case_categ_claim0'
         )
-        result['domain'] = "[('id', 'in', %s)]" % self.claim_ids.ids
+        result['domain'] = "[('id', 'in', %s)]" % self.claim_ids
         return result
